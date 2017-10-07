235cc6d272c23b2354f04e0fe9fe84d36c7f9c6c
logiclogue <jordanpaullord@hotmail.co.uk>
2017-10-07 18:52:57 +0100

    Adds empty expression class

---
+++ src/Expression.js
@@ -0,0 +1,19 @@
+function Expression() {
+    
+}
+
+Expression.prototype = {
+
+    // Expression ~> String -> Object -> (Boolean, Boolean)
+    interpret: function (input, values) {
+        $
+    },
+
+    // Expression ~> String -> Boolean
+    validate: function (input) {
+        $
+    }
+    
+};
+
+module.exports = Expression;
