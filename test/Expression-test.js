aa0280e7a140bcca16c5d2f79eff2025b1ac5c80
logiclogue <jordanpaullord@hotmail.co.uk>
2017-10-08 21:59:37 +0100

    Adds empty test for expression

---
+++ test/Expression-test.js
@@ -0,0 +1,18 @@
+var expect = require("chai").expect;
+var Expression = require("../src/Expression");
+
+describe("Expression", function () {
+    var expression;
+
+    before(function () {
+        expression = new Expression();
+    });
+
+    describe("#interpret()", function () {
+
+    });
+
+    describe("#validate", function () {
+
+    });
+});
